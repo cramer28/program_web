17630b817d74fd45eaef62e38fdb440013557622
Geronimo <cramer9858@outlook.com>
2021-08-23 00:31:48 -0300

    cambios al practico 2

--- listareas/listareas.js
+++ listareas/listareas.js
@@ -1,50 +1,49 @@
 const inputs = document.querySelector(".ingresar input");
 const boton = document.querySelector(".ingresar button");
 const lista = document.querySelector(".lista");
-let id = 0;
-let tarea = "";
-let emoji = String.fromCodePoint(0x1F5D1);
+let borrarIcn = String.fromCodePoint(0x1F5D1);
+let compartirIcn = String.fromCodePoint(0x2BAB);
 
-inputs.onkeyup = ()=>{
+inputs.addEventListener("keyup", function(event) {
     let valorIngresado = inputs.value;
     if(valorIngresado.trim() != 0){
-        boton.classList.add("active"); 
+        if (event.code === 'Enter') {
+            agregar();
+        }else{
+            boton.classList.add("active"); 
+        }
     }else{
         boton.classList.remove("active");
     }
-}
-
-
+});
 
 boton.onclick = ()=>{
-    var f=id;
-    let texto = inputs.value;
+    agregar();
+}
+
+function agregar(){
     var li = document.createElement("li");
+    var check = document.createElement("input");
+    var text = document.createElement("p");
     var but = document.createElement("span");
-    li.appendChild(document.createTextNode(`${texto}`));
-    li.setAttribute("id", f);
-    li.onclick = function(){tildar(f)};
-    but.appendChild(document.createTextNode(emoji));
+    var com = document.createElement("span");
+    text.appendChild(document.createTextNode(inputs.value));
+    check.setAttribute("type","checkbox");
+    but.appendChild(document.createTextNode(borrarIcn));
     but.setAttribute("class", "eliminar");
-    but.onclick = function(){borrar(f)};
+    but.onclick = function(){this.parentElement.remove();};
+    com.appendChild(document.createTextNode(compartirIcn));
+    com.setAttribute("class", "compartir");
+    com.onclick = function(){compartir(this.parentElement);};
+    li.appendChild(check);
+    li.appendChild(text);
+    li.appendChild(com);
     li.appendChild(but);
-    //lista.appendChild(li);
     lista.insertBefore(li, lista.childNodes[0]);
     inputs.value = "";
-    id++;
     boton.classList.remove("active");
 }
 
-function borrar(f){
-    var aBorrar = document.getElementById(f);
-    aBorrar.remove();
-}
-
-function tildar(f){
-    let taria = document.getElementById(f);
-    if(taria.classList.contains("tilde")){
-        taria.classList.remove("tilde");
-    }else{
-        taria.classList.add("tilde");
-    }
+function compartir(tarea){
+    alert(tarea.value);
 }
